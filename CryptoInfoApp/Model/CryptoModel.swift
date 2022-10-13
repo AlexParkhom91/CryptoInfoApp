ac3f5eca006f57d974b476fc92c311923eaa7246
AlexParkhom91 <parkhom91work@gmail.com>
2022-10-13 16:56:24 +0300

    add activity load & DetailVC

--- CryptoInfoApp/Model/CryptoModel.swift
+++ CryptoInfoApp/Model/CryptoModel.swift
@@ -1,21 +1,13 @@
-//
-//  CryptoModel.swift
-//  CryptoInfoApp
-//
-//  Created by Александр Пархамович on 9.10.22.
-//
-
 import Foundation
 
 struct CryptoModel: Codable {
     let name: String
     let price: Double?
     let id: String
-    
-    enum CodingKeys: String,CodingKey {
+
+    enum CodingKeys: String, CodingKey {
         case name
         case price = "price_usd"
         case id = "asset_id"
     }
 }
-
